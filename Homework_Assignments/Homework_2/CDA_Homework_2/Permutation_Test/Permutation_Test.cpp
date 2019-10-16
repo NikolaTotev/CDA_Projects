2a6c63cb6bc86cd3c6b8349038f6575e1dd723e3
Nikola <nikola.r.totev@gmail.com>
2019-10-17 02:27:19 +0300

    Started homework 2

---
+++ Homework_Assignments/Homework_2/CDA_Homework_2/Permutation_Test/Permutation_Test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	string wordOne;
+	string wordTwo;
+	int wordLenth;
+
+	cin >> wordLenth;
+	cin >> wordOne;
+	cin >> wordTwo;
+
+	
+	vector<int> wordArr1;
+	vector<int> wordArr2;
+
+	for (int i = 0; i < UPPER; ++i)
+	{
+		
+
+		if(wordOne.Ha)
+	}
+
+	for (int i = 0; i < wordLenth; ++i)
+	{
+		
+	}
+}
+
