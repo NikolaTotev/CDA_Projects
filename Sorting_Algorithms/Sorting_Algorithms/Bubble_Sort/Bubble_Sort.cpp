0377d890dd9fdb8486e1e2fe3222bce8143e152d
Nikola <nikola.r.totev@gmail.com>
2019-10-19 10:34:42 +0300

    Implemented bubble sort.

---
+++ Sorting_Algorithms/Sorting_Algorithms/Bubble_Sort/Bubble_Sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int arr_1[10] = {7,5,3,2,6,2,3,4,1,2};
+
+
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int j = 0; j < 10-i-1; ++j)
+		{
+			if(arr_1[j]>arr_1[j+1])
+			{
+				int temp = arr_1[j + 1];
+				arr_1[j + 1] = arr_1[j];
+				arr_1[j] = temp;
+			}
+		}
+	}
+
+
+	for (int i = 0; i < 10; ++i)
+	{
+		cout << arr_1[i] << endl;
+	}
+
+
+}
