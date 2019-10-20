2cf9aba0e192c72a06591a021d489e8108d52056
Nikola <nikola.r.totev@gmail.com>
2019-10-20 21:44:55 +0300

    Added problem 5 from hw 1 & hw 2

---
+++ Homework_Assignments/Homework_2/CDA_Homework_2/Problem_4/Problem_4.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	long long numberOfActivities = 0;
+	cin >> numberOfActivities;
+
+	vector<vector <long long>> activityInfo;
+
+	string line;
+	getline(cin, line);
+
+	for (int i = 0; i < numberOfActivities; ++i)
+	{
+		int number;
+		vector<long long> activityPair;
+
+		getline(std::cin, line);
+		istringstream stream(line);
+		while (stream >> number)
+			activityPair.push_back(number);
+
+		activityInfo.push_back(activityPair);
+	}
+
+	int activityCounter = 1;
+	int currentSum  = activityInfo[0][0] + activityInfo[0][1];;
+	for (int i = 0; i < numberOfActivities-1; ++i)
+	{
+		
+		if (currentSum <= activityInfo[i + 1][0])
+		{
+			currentSum = activityInfo[i+1][0] + activityInfo[i+1][1];
+			activityCounter++;
+		}
+	}
+
+	cout << activityCounter;
+}
+
+
