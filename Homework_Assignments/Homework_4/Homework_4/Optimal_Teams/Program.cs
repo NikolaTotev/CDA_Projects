7e50f07d0dca0cd079c0520c444141c1753f20bc
Nikola <nikola.r.totev@gmail.com>
2019-11-10 16:04:48 +0200

    Finisehd Welcome to the jungle

---
+++ Homework_Assignments/Homework_4/Homework_4/Optimal_Teams/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using longBoi = System.Int32;
+
+struct studentInfo
+{
+    public longBoi position;
+    public longBoi numberOfSkills;
+
+}
+namespace Optimal_Teams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string initValues = Console.ReadLine();
+            string initFixed = initValues.Trim(' ');
+
+            string[] initNumbers = initFixed.Split(' ');
+            int[] numbersConverted = Array.ConvertAll(initNumbers, int.Parse);
+            longBoi numberOfStudents = numbersConverted[0];
+            longBoi selectionRange = numbersConverted[1];
+
+            string rawNumbers = Console.ReadLine();
+            string Fixed = rawNumbers.Trim(' ');
+            string[] numbers = Fixed.Split(' ');
+            
+            List<longBoi> studentSkills = Array.ConvertAll(numbers, longBoi.Parse).ToList();
+            List<longBoi> sortedSkills = Array.ConvertAll(numbers, longBoi.Parse).ToList();
+            List<longBoi> tempList = Array.ConvertAll(numbers, longBoi.Parse).ToList();
+            sortedSkills.Sort();
+
+            Dictionary<int, longBoi> studentInfo = new Dictionary<int, longBoi>();
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                studentInfo.Add(sortedSkills[i], studentSkills.IndexOf(sortedSkills[i]));
+            }
+
+            longBoi studentsPlaced = 0;
+            bool isIvansTurn = true;
+
+            while (studentsPlaced < numberOfStudents)
+            {
+                longBoi currentMax = studentInfo.ElementAt(studentInfo.Count - 1).Key;
+                longBoi indexOfCurrentMax = studentInfo.ElementAt(studentInfo.Count - 1).Value;
+                studentInfo.Remove(currentMax);
+                if (!isIvansTurn)
+                {
+                    studentSkills[indexOfCurrentMax] = -2;
+                }
+                else
+                {
+                    studentSkills[indexOfCurrentMax] = -1;
+                }
+
+                studentsPlaced++;
+                for (int i = indexOfCurrentMax + 1; i <= indexOfCurrentMax + selectionRange && i < tempList.Count; i++)
+                {
+                    if (studentSkills[i] != -1 && studentSkills[i] != -2)
+                    {
+                        studentInfo.Remove(studentSkills[i]);
+                        tempList.Remove(studentSkills[i]);
+                        if (!isIvansTurn)
+                        {
+                            studentSkills[i] = -2;
+                        }
+                        else
+                        {
+                            studentSkills[i] = -1;
+                        }
+                        studentsPlaced++;
+                        //continue;
+                    }
+
+                  //  break;
+                }
+
+                for (int i = indexOfCurrentMax - 1; i >= indexOfCurrentMax - selectionRange && i >= 0; i--)
+                {
+                    if (studentSkills[i] != -1 && studentSkills[i] != -2)
+                    {
+                        studentInfo.Remove(studentSkills[i]);
+
+                        if (!isIvansTurn)
+                        {
+                            studentSkills[i] = -2;
+                        }
+                        else
+                        {
+                            studentSkills[i] = -1;
+                        }
+
+                        studentsPlaced++;
+                        //continue;
+                    }
+
+                   // break;
+                }
+
+                isIvansTurn = !isIvansTurn;
+            }
+
+            foreach (var skill in studentSkills)
+            {
+                if (skill == -1)
+                {
+                    Console.Write(1);
+                }
+                else
+                {
+                    Console.Write(2);
+                }
+            }
+        }//Main end
+
+    }
+}
--- Homework_Assignments/Homework_4/Homework_4/Welcome_To_The_Jungle/Welcome_To_The_Jungle.cpp
+++ Homework_Assignments/Homework_4/Homework_4/Welcome_To_The_Jungle/Welcome_To_The_Jungle.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <stack>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 typedef  long long longBoi;
 using namespace std;
+struct stackInfo
+{
+	longBoi min;
+	longBoi max;
+	longBoi count;
+	longBoi position;
+};
 int main()
 {
 	longBoi numberOfTrees;
@@ -14,6 +25,65 @@ int main()
 		treeheights[i] = input;
 	}
 
-	std::cout << "Hello World!\n";
+	stack<longBoi> trees;
+	vector<stackInfo*> infoHistory;
+	stackInfo* currentInfo = new stackInfo;
+	currentInfo->count = 1;
+	currentInfo->max = treeheights[numberOfTrees - 1];
+	currentInfo->min = treeheights[numberOfTrees - 1];
+	currentInfo->position = numberOfTrees - 1;
+	infoHistory.push_back(currentInfo);
+
+	for (int i = numberOfTrees - 2; i >= 0; --i)
+	{
+		longBoi currentTreeHeight = treeheights[i];
+		if (currentTreeHeight < currentInfo->min)
+		{
+			currentInfo->count++;
+			currentInfo->position = i;
+			currentInfo->min = currentTreeHeight;
+			trees.push(currentTreeHeight);
+
+			continue;
+		}
+
+		stackInfo* newInfo = new stackInfo;
+		while (!trees.empty() && trees.top() <= currentTreeHeight)
+		{
+			trees.pop();
+		}
+		trees.push(currentTreeHeight);
+		newInfo->min = currentTreeHeight;
+		if (currentTreeHeight >= currentInfo->max)
+		{
+			newInfo->max = currentTreeHeight;
+		}
+		else
+		{
+			newInfo->max = currentInfo->max;
+		}
+		newInfo->position = i;
+		newInfo->count = trees.size();
+		infoHistory.push_back(newInfo);
+		currentInfo = newInfo;
+
+
+	}
+
+	longBoi indexWithMaxCount = infoHistory[0]->position;
+	longBoi maxCount = infoHistory[0]->count;
+
+	for (int i = 0; i < infoHistory.size(); ++i)
+	{
+		if (infoHistory[i]->count >= maxCount)
+		{
+			maxCount = infoHistory[i]->count;
+			indexWithMaxCount = infoHistory[i]->position;
+		}
+	}
+
+	
+	cout << indexWithMaxCount;
+
 }
 
